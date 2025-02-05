d82a0ed6379d50c0df080a49862a4ad910932875
Sindhura <168758473+SindhuraBanapuram@users.noreply.github.com>
2025-02-05 13:35:15 +0530

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 app = Flask(__name__, template_folder='templates')
 CORS(app)
 
-@app.route('/https://webscraping-to-csv-file.onrender.com')
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -79,4 +79,5 @@ def scrape_data(url):
         return None
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
